c86c0be20f09b45c0ba0f461928e4dd50378c6de
Jone-one <1259922325@qq.com>
2018-10-11 11:00:34 +0800

    单独提交

--- src/main/java/example/controller/myredis.java
+++ src/main/java/example/controller/myredis.java
@@ -3,5 +3,6 @@ package example.controller;
 public class myredis {
     public static void main(String[] args) {
         System.out.println("github push test");
+        System.out.println("单独提交");
     }
 }
